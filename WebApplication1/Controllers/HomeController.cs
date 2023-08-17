f4c6c726a2806c3298b2e5538c53d96eba620511
boxuen <s21101114@stu.edu.tw>
2023-08-17 23:27:19 +0800

    20230817修改 已完成權限分配

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -26,5 +26,19 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+
+        public ActionResult AdminDashboard()
+        {
+            return View(); // 這個操作方法對應到 Admin 儀表板頁面
+        }
+
+        public ActionResult ManagerDashboard()
+        {
+            return View(); // 這個操作方法對應到 Manager 儀表板頁面
+        }
+        public ActionResult PlayerDashboard()
+        {
+            return View(); // 這個操作方法對應到 Player 儀表板頁面
+        }
     }
 }
--- WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/Controllers/LoginController.cs
@@ -1,24 +1,53 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class LoginController : Controller
     {
+        private NEW2101Entities1 db = new NEW2101Entities1();
 
-        // GET: Login
-       
         public ActionResult Index()
         {
             return View();
         }
-       
-        
-       
-       
+
+        [HttpPost]
+        public ActionResult Login(string Account, string Password)
+        {
+            var existingUser = db.AccountData.FirstOrDefault(u => u.Account == Account && u.Password == Password);
+
+            if (existingUser != null)
+            {
+                Session["Username"] = existingUser.Username;
+
+                if (existingUser.Role_ID == 1) // Admin
+                {
+                    Debug.WriteLine("User is an Admin.");
+                    return RedirectToAction("AdminDashboard", "Home");
+                }
+                else if (existingUser.Role_ID == 2) // Manager
+                {
+                    Debug.WriteLine("User is a Manager.");
+                    return RedirectToAction("ManagerDashboard", "Home");
+                }
+                else if (existingUser.Role_ID == 3) // Player
+                {
+                    Debug.WriteLine("User is a Player.");
+                    return RedirectToAction("PlayerDashboard", "Home");
+                }
+            }
+
+            ModelState.AddModelError("error", "無效的用戶名或密碼");
+            return View("Index");
+        }
     }
 }
