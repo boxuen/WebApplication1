1c2c5e6c1e1794b33c9390c9296e9d5bfdf8edfa
ANHELNANA <124913560+ANHELNANA@users.noreply.github.com>
2023-08-26 23:59:11 +0800

    1120826完成帳號管理

--- WebApplication1/Controllers/AccountDatasController.cs
+++ WebApplication1/Controllers/AccountDatasController.cs
@@ -8,7 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
 
-namespace WebApplication1.Views
+namespace WebApplication1.Controllers
 {
     public class AccountDatasController : Controller
     {
@@ -44,7 +44,7 @@ namespace WebApplication1.Views
         }
 
         // POST: AccountDatas/Create
-        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
         // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -78,7 +78,7 @@ namespace WebApplication1.Views
         }
 
         // POST: AccountDatas/Edit/5
-        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
         // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
